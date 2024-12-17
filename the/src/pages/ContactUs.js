4cd715f1030f197eeda9e847a35db244a993333b
Ivana Carrillo <ivanae.carrillo@gmail.com>
2024-12-17 11:05:17 -0500

    added contact page and links

--- the/src/pages/ContactUs.js
+++ the/src/pages/ContactUs.js
@@ -1,7 +1,84 @@
 import React from "react";
 
 const ContactUs = () => {
-  return <h1>Contact Us</h1>;
+  return (
+    <div
+      style={{
+        backgroundColor: "black", // Background color for the page
+        color: "white",
+        minHeight: "100vh", // Full viewport height
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center", // Center vertically
+        alignItems: "center", // Center horizontally
+        textAlign: "center",
+        padding: "20px",
+      }}
+    >
+      <h1 style={{ marginBottom: "20px" }}>Contact Us</h1>
+      <div style={{ lineHeight: "2" }}>
+        {/* Clickable Email */}
+        <p>
+          Email:{" "}
+          <a
+            href="mailto:gianaaragon@gmail.com"
+            style={{
+              color: "white",
+              textDecoration: "none",
+            }}
+          >
+            gianaaragon@gmail.com
+          </a>
+        </p>
+
+        {/* Clickable Phone */}
+        <p>
+          Phone:
+          <a
+            href="tel:4135312171"
+            style={{
+              color: "white",
+              textDecoration: "none",
+            }}
+          >
+            413-531-2171
+          </a>
+        </p>
+
+        {/* Clickable Address */}
+        <p>
+          Address:{" "}
+          <a
+            href="https://www.google.com/maps?q=6+N+Main+St,+East+Longmeadow,+MA"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              color: "white",
+              textDecoration: "none",
+            }}
+          >
+            6 N Main St. East Longmeadow, MA
+          </a>
+        </p>
+
+        {/* Clickable Website */}
+        <p>
+          Website:{" "}
+          <a
+            href="https://www.gogallagher.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              color: "white",
+              textDecoration: "none",
+            }}
+          >
+            www.gogallagher.com
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ContactUs;
